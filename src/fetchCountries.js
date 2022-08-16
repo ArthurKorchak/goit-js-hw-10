c95364fb8da71c68c330a88f5d64623364c0548f
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-16 22:40:15 +0300

    upd

--- src/fetchCountries.js
+++
@@ -1,9 +0,0 @@
-export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(resp => {
-            if (!resp.ok) {
-                throw new Error(resp.status);
-            };
-            return resp.json();
-        });
-};
